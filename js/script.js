123c2ea77e54c9c959b0eb0c17bd33fff69e63d0
Oliver Kelso <oliver@pixelsoul.com>
2018-04-30 13:45:48 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+Vue.filter('date', time => moment(time)
+  .format('MMM DD, YYYY, HH:mm a'))
+
 new Vue({
 
   el: '#notebook',
@@ -36,6 +39,10 @@ new Vue({
       }
     },
 
+    favoriteNote(){
+      this.selectedNote.favorite = !this.selectedNote.favorite
+    },
+
     // retrieve note
     selectNote(note){
       this.selectedId = note.id
@@ -53,14 +60,49 @@ new Vue({
   },
 
   computed: {
+    // note preview
     notePreview(){
       return this.selectedNote ? marked(this.selectedNote.content) : ''
     },
+    // note count
     addButtonTitle(){
       return this.notes.length + ' note(s) added'
     },
+    // selected note
     selectedNote(){
       return this.notes.find(note => note.id === this.selectedId)
+    },
+    // sort notes sidebar
+    sortedNotes(){
+      return this.notes.slice()
+        .sort((a, b) => a.created - b.created)
+        .sort((a, b) => (a.favorite === b.favorite) ? 0
+          : a.favorite? -1
+          : 1)
+    },
+    // line count
+    linesCount(){
+      if(this.selectedNote){
+        return this.selectedNote.content.split(/\r\n|\r|\n/).length
+      }
+    },
+    // word count
+    wordsCount(){
+      if(this.selectedNote){
+        var s = this.selectedNote.content
+        // turn new line characters into white space
+        s = s.replace(/\n/g, ' ')
+        // exclude start and end white spaces
+        s = s.replace(/(^\s*) | (\s*$)/gi, '')
+        // turn 2 or more duplicate white space into 1
+        s = s.replace(/\s\s+/gi, ' ')
+        // return the number of spaces
+        return s.split(' ').length
+      }
+    },
+    // character count
+    charactersCount(){
+      return this.selectedNote.content.split(' ').length
     },
   },
 
